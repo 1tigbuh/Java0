70727b2ca1f266f779f4888776854fa54fd9203b
Ilya <ilyakolesnik2048@gmail.com>
2025-10-30 20:45:52 +0200

    methods

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,69 +1,81 @@
 package app;
 
-import java.util.Random;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int[][] matrix = new int[4][4];
-        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                matrix[i][j] = rand.nextInt(50) + 1;
-            }
-        }
-        for (int i = 0; i < 4;i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.printf("%3d ", matrix[i][j]);
-            }
-            System.out.println();
-        }
-        int evenrows = 0;
-        int oddrows = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (i % 2 == 0) evenrows += matrix[i][j];
-                else oddrows += matrix[i][j];
-            }
-        }
-        long evencols = 1;
-        long oddcols = 1;
-        for (int j = 0;j < 4; j++) {
-            for (int i = 0; i < 4; i++) {
-                if (j % 2 == 0) evencols *= matrix[i][j];
-                else oddcols *= matrix[i][j];
-            }
-        }
+        System.out.print("Введіть ціле число: ");
+        int numb = sc.nextInt();
+        Square(numb);
 
-        System.out.println("Сума парних рядків: " + evenrows);
-        System.out.println("Сума непарних рядків: " + oddrows);
-        System.out.println("Добуток парних стовпців: " + evencols);
-        System.out.println("Добуток непарних стовпців: " + oddcols);
 
-        int magsum = 0;
-        for (int j = 0; j < 4; j++) {
-            magsum += matrix[0][j];
-        }
-        boolean magic = true;
-        for (int i = 1; i < 4 && magic; i++) {
-            int rowsum = 0;
-            for (int j = 0; j < 4; j++) rowsum += matrix[i][j];
-            if (rowsum != magsum) magic = false;
-        }
-        for (int j = 0; j < 4 && magic; j++) {
-            int sumcol = 0;
-            for (int i = 0; i < 4; i++) sumcol += matrix[i][j];
-            if (sumcol != magsum) magic = false;
-        }
-        int diag1 = 0, diag2 = 0;
-        for (int i = 0; i < 4; i++) {
-            diag1 += matrix[i][i];
-            diag2 += matrix[i][3 - i];
-        }
-        if (diag1 != magsum || diag2 != magsum) magic = false;
+        System.out.print("Введіть радіус: ");
+        double R = sc.nextDouble();
+        System.out.print("Введіть висоту: ");
+        double h = sc.nextDouble();
+        System.out.println("Об'єм циліндра з радіусом " + R + " і висотою " + h + " дорівнює " + Vol(R, h) + ".");
 
-        if (magic)
-            System.out.println("Матриця є магічним квадратом.");
-        else
-            System.out.println("Матриця не є магічним квадратом.");
+
+        System.out.print("Введіть кількість елементів масиву:");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.print("Введіть елемент " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Масив чисел: " + Arrays.toString(arr));
+        int sum = SumArr(arr);
+        System.out.println("Сума всіх елементів масиву дорівнює " + sum + ".");
+
+
+        sc.nextLine();
+        System.out.print("Введіть рядок: ");
+        String text = sc.nextLine();
+        System.out.println("Рядок в зворотньому порядку: " + ReverseStr(text));
+
+
+        System.out.print("Введіть a: ");
+        int a = sc.nextInt();
+        System.out.print("Введіть b: ");
+        int b = sc.nextInt();
+        System.out.println("Результат " + a + "^" + b + " дорівнює " + power(a, b) + ".");
+
+
+        System.out.print("Введіть ціле число n: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Введіть текстовий рядок:");
+        String t = sc.nextLine();
+        RepeatTxt(n, t);
+    }
+
+
+    public static void Square(int n) {
+        System.out.println("Квадрат числа " + n + " дорівнює " + (n * n) + ".");
+    }
+
+    public static double Vol(double R, double h) {
+        return Math.PI * R * R * h;
+    }
+
+    public static int SumArr(int[] arr) {
+        int sum = 0;
+        for (int numb : arr) sum += numb;
+        return sum;
+    }
+
+    public static String ReverseStr(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    public static int power(int a, int b) {
+        return (int) Math.pow(a, b);
+    }
+
+    public static void RepeatTxt(int n, String text) {
+        for (int i =0; i < n; i++) System.out.println(text);
     }
 }
